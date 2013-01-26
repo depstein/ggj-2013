621e8c607c87fd2b218fc30c77bae3f12ddaef6c
Andrew Gaubatz <atgaubatz@gmail.com>
2013-01-26 15:20:31 -0800

    Added sample level definitions.

---
+++ assets/levels/LevelDefinition.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		blob = 'blob1',
+		rotation = 110,
+		scale = 1.5,
+		layer = SceneManager.THE_BEST_LAYER,
+		color = "ffffff"
+	}
+	{
+		blob = 'blob17',
+		rotation = 200,
+		scale = 1.1,
+		layer = SceneManager.FOREGROUND,
+		color = "ffffdd"
+	}
+}
