10ff299c064fab7d4e929e9977c5f7bc7418461a
depstein <depstein@cs.washington.edu>
2013-01-26 02:28:14 -0800

    Added a rope class

--- main.lua
+++ main.lua
@@ -10,6 +10,7 @@ require "GameObject"
 require "PhysicsGameObject"
 require "Character"
 require "Player"
+require "Rope"
 
 
 SceneManager:new():init(1024, 768)
@@ -28,31 +29,5 @@ local testObject4 = PhysicsGameObject:new():init(TextureAsset.get("whitesquare.p
 testObject4.handle:setPos(-200, -200)
 testObject4:setColor(Colors.rhythm)
 
-local rope1 = PhysicsGameObject:new():init(TextureAsset.get("whiterectangle.png"));
-rope1.handle:setPos(-200, -160)
-rope1:setColor(Colors.silver)
-
-
-local rope2 = PhysicsGameObject:new():init(TextureAsset.get("whiterectangle.png"));
-rope2.handle:setPos(-200, -140)
-rope2:setColor(Colors.shocking_pink_crayola)
-
-local rope3 = PhysicsGameObject:new():init(TextureAsset.get("whiterectangle.png"));
-rope3.handle:setPos(-200, -120)
-rope3:setColor(Colors.smokey_topaz)
-
-
-mouseJoint = MOAICpConstraint.newPivotJoint (testObject4.body, rope1.body, 0, 0, -25, 0)
-mouseJoint:setMaxForce ( 5000 )
-mouseJoint:setBiasCoef ( 0.15 )
-SceneManager.i:getCpSpace():insertPrim ( mouseJoint )
-
-mouseJoint2 = MOAICpConstraint.newPivotJoint (rope1.body, rope2.body, 25, 0, -25, 0)
-mouseJoint2:setMaxForce ( 5000 )
-mouseJoint2:setBiasCoef ( 0.15 )
-SceneManager.i:getCpSpace():insertPrim ( mouseJoint2 )
-
-mouseJoint3 = MOAICpConstraint.newPivotJoint (rope2.body, rope3.body, 25, 0, -25, 0)
-mouseJoint3:setMaxForce ( 5000 )
-mouseJoint3:setBiasCoef ( 0.15 )
-SceneManager.i:getCpSpace():insertPrim ( mouseJoint3 )
+local rope = Rope:new():init(-200, -200, 5);
+rope:setStartBody(testObject4);
---
+++ src/GameObjects/Rope.lua
@@ -0,0 +1,32 @@
+Rope = {}
+Rope.type = "Rope"
+
+function Rope:new(o)
+	o = o or {}
+	setmetatable(o, {__index = self})
+	return o
+end
+
+function Rope:init(x, y, numRopeSegments) 
+	self.ropeSegments = {}
+	for i=1, numRopeSegments do
+		segment = PhysicsGameObject:new():init(TextureAsset.get("whiterectangle.png"));
+		segment.handle:setPos(x-50*i, y)
+		segment:setColor(Colors.coral_red)
+		self.ropeSegments[i] = segment
+		if (i>1) then
+			joint = MOAICpConstraint.newPivotJoint (self.ropeSegments[i-1].body, self.ropeSegments[i].body, -25, 0, 25, 0)
+			joint:setMaxForce ( 5000 )
+			joint:setBiasCoef ( 0.75 )
+			SceneManager.i:getCpSpace():insertPrim ( joint )
+		end
+	end
+	return self
+end
+
+function Rope:setStartBody(startObject)
+	joint = MOAICpConstraint.newPivotJoint (startObject.body, self.ropeSegments[1].body, 0, 0, 25, 0)
+			joint:setMaxForce ( 5000 )
+			joint:setBiasCoef ( 0.75 )
+			SceneManager.i:getCpSpace():insertPrim ( joint )
+end
