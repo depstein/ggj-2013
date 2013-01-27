32076dbedfd4b7b8bc45cf4a772890e31bb97182
depstein <depstein@cs.washington.edu>
2013-01-26 22:27:32 -0800

    OMG YOU CAN DROP YOUR ROPE IN A DROP LOCATION

---
+++ src/GameObjects/DropLocation.lua
@@ -0,0 +1,43 @@
+require "PhysicsGameObject"
+
+DropLocation = PhysicsGameObject:new()
+DropLocation.type = "DropLocation"
+
+function DropLocation:init(asset, options)
+	options = options or {}
+	options.static = true
+	options.isSensor = true
+	PhysicsGameObject.init(self, asset, options)
+	self:createPhysicsObject(options)
+
+	self:setType(tableaddr(DropLocation))
+
+	self:setColor(Game.colors.yellow)
+
+	Game.sceneManager:getCpSpace():setCollisionHandler(tableaddr(DropLocation), tableaddr(Rope), MOAICpSpace.BEGIN, self.collideWithRope)
+	return self
+end
+
+function DropLocation:collideWithRope(cpShapeA, cpShapeB, cpArbiter)
+	print("HERE")
+
+	if not cpArbiter:isFirstContact() then
+		return
+	end
+
+	for k, v in pairs(cpShapeA:getBody().gameObject.joints) do 
+		Game.sceneManager:getCpSpace():removePrim(v)
+	end
+	for k, v in pairs(cpShapeB:getBody().gameObject.joints) do 
+		Game.sceneManager:getCpSpace():removePrim(v)
+	end
+
+	joint = MOAICpConstraint.newSlideJoint(cpShapeA:getBody(), cpShapeB:getBody(), 0, 0, 0, 0, 0, 50)
+	joint:setBiasCoef ( 0.75 )
+
+	Game.sceneManager:getCpSpace():insertPrim ( joint )
+	cpShapeA:getBody().gameObject:setGroup(tableaddr(Rope))
+	table.insert(cpShapeA:getBody().gameObject.joints, joint)
+	cpShapeB:getBody().gameObject:setGroup(tableaddr(Rope))
+	table.insert(cpShapeB:getBody().gameObject.joints, joint)
+end
